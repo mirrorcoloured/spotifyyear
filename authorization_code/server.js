fa9dd517130067b1f901c7cc0bbf8426f7efb1a6
mirrorcoloured <mirrorcoloured@gmail.com>
2023-10-04 21:56:35 -0600

    updated uri for azure

--- authorization_code/server.js
+++ authorization_code/server.js
@@ -20,9 +20,13 @@ const client_id = process.env.spotifyyear_client_id;
 const client_secret = process.env.spotifyyear_client_secret;
 console.log('running app with client id', client_id, 'client secret', client_secret);
 
-const uri = `http://localhost:${listening_port}`;
+// const uri = `http://localhost:${listening_port}`;
 // const uri = `http://pjsc.me:${listening_port}`;
+<<<<<<< HEAD
 const uri = `https://spotify-this-year.azurewebsites.net`;
+=======
+const uri = `http://spotify-this-year.azurewebsites.net`;
+>>>>>>> 4de7fc8 (updated uri for azure)
 const redirect_uri = `${uri}/callback`; // Your redirect uri
 
 /**

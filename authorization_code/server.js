6a4d626d927485cf2d7a1b7125860b6b3bcbe7c2
mirrorcoloured <mirrorcoloured@gmail.com>
2023-10-04 22:14:39 -0600

    update to https

--- authorization_code/server.js
+++ authorization_code/server.js
@@ -22,7 +22,7 @@ console.log('running app with client id', client_id, 'client secret', client_sec
 
 const uri = `http://localhost:${listening_port}`;
 // const uri = `http://pjsc.me:${listening_port}`;
-// const uri = `http://spotify-this-year.azurewebsites.net`;
+const uri = `https://spotify-this-year.azurewebsites.net`;
 const redirect_uri = `${uri}/callback`; // Your redirect uri
 
 /**
